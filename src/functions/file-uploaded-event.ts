bd1cfe0b0f5dc7dcf8d6e4eab071536772612409
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-07-19 18:33:03 -0300

    feat: integrate SQS client and send messages for uploaded files

--- src/functions/file-uploaded-event.ts
+++ src/functions/file-uploaded-event.ts
@@ -1,8 +1,17 @@
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
 import { S3Event } from "aws-lambda";
-import { record } from "zod";
+
+import { sqsClient } from "../client/sqs-client";
 
 export async function handler(event: S3Event) {
- await Promise.all(event.Records.map(record => {
-  record.s3.object.key
+ await Promise.all(event.Records.map(async record => {
+    const command = new SendMessageCommand({
+      QueueUrl: process.env.MEALS_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        fileKey: record.s3.object.key,
+
+      })
+    })
+    await sqsClient.send(command)
  }))
 } 
